c19be61a3daccd5617793ae0697255061cf21409
Shun Ueda <hasundue@gmail.com>
2022-04-22 09:55:17 +0900

    Remove tests

--- kvtool_test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
-import { readConfig } from "./kvtool.ts";
-
-Deno.test("readConfig", async () => {
-  const config = await readConfig("./test/wrangler.toml");
-  assertEquals(config.accountId, "00000000000000000000000000000000");
-  assertEquals(config.apiToken, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-});
